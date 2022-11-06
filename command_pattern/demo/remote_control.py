a6300e3eea3fd00032e26cd8760541bac11453be
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-11-06 22:00:16 +0000

    Refactor remote_control.py to include undo button

--- command_pattern/demo/remote_control.py
+++ command_pattern/demo/remote_control.py
@@ -1,18 +1,19 @@
 from commands import *
 
 class SimpleRemoteControl:
-    def __init__(self, no_of_slots: int)-> None:
+    def __init__(self, no_of_slots: int, no_command: NoCommand = NoCommand())-> None:
         self.no_of_slots = no_of_slots
-        self.on_commands: list = [None] * self.no_of_slots
-        self.off_commands: list = [None] * self.no_of_slots
+        self.on_commands: list = [no_command] * self.no_of_slots
+        self.off_commands: list = [no_command] * self.no_of_slots
+        self.undo_command : Command = no_command
 
     def __str__(self):
         string_buffer: list = []
         string_buffer.append("\n------------- Remote Control --------------\n")
 
         for i in range(self.no_of_slots):
-            on_command = self.on_commands[i].__class__.__name__ if self.on_commands[i].__class__.__name__ != "NoneType" else "No Command"
-            off_command = self.off_commands[i].__class__.__name__ if self.off_commands[i].__class__.__name__ != "NoneType" else "No Command"
+            on_command = self.on_commands[i].__class__.__name__
+            off_command = self.on_commands[i].__class__.__name__
             string_buffer.append("[slot " + str(i) + "] " + on_command + "    " + off_command + "\n")
 
         return " ".join(str(e) for e in string_buffer)
@@ -23,6 +24,11 @@ class SimpleRemoteControl:
 
     def on_button_was_pressed(self, slot_index: int):
         self.on_commands[slot_index].execute()
+        self.undo_command = self.on_commands[slot_index]
 
     def off_button_was_pressed(self, slot_index: int):
-        return self.off_commands[slot_index].execute()
+        self.off_commands[slot_index].execute()
+        self.undo_command = self.off_commands[slot_index]
+
+    def undo_button_was_pressed(self):
+        self.undo_command.undo()
