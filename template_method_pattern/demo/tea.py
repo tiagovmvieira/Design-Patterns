3a5811502f113882553f05cb568427b50bb2ed4a
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-11-19 21:13:49 +0000

    Add TeaWithHook class included on the coding barista demo

--- template_method_pattern/demo/tea.py
+++ template_method_pattern/demo/tea.py
@@ -1,4 +1,5 @@
 from caffeine_beverage import CaffeineBeverage, CaffeineBeverageWithHook
+from termcolor import colored
 
 class Tea(CaffeineBeverage):
     def __init__(self):
@@ -10,7 +11,7 @@ class Tea(CaffeineBeverage):
     def _add_condiments(self):
         print("Adding lemon")
 
-class TeaWithUserInteraction(CaffeineBeverageWithHook):
+class TeaWithHook(CaffeineBeverageWithHook):
     def __init__(self):
         super().__init__()
 
@@ -19,3 +20,17 @@ class TeaWithUserInteraction(CaffeineBeverageWithHook):
 
     def _add_condiments(self):
         print("Adding lemon")
+
+    def _get_user_input(self)-> str:
+        user_answer = None
+        user_answer = str(input(colored("Would you like lemon with your tea (y/n)? ", "red")))
+
+        return user_answer
+
+    def _customer_wants_condiments(self) -> bool:
+        self.user_answer: str = self._get_user_input()
+
+        if self.user_answer.lower().startswith("y"):
+            return True
+        else:
+            return False
