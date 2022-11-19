bf366d909df1041ca152f56a9d74826bb291354a
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-11-19 21:15:30 +0000

    Add CaffeineBeverageWithHook class and corresponding hook method

--- template_method_pattern/demo/caffeine_beverage.py
+++ template_method_pattern/demo/caffeine_beverage.py
@@ -29,3 +29,35 @@ class CaffeineBeverage(ABC):
     @final
     def _pour_in_cup(self):
         print("Pouring into cup")
+
+class CaffeineBeverageWithHook(ABC):
+    def __init__(self):
+        if self.__class__ == CaffeineBeverageWithHook:
+            raise TypeError("Instantiating the Abstract Class")
+
+    @final
+    def prepare_recipe(self) -> None:
+        self._boil_water()
+        self._brew()
+        self._pour_in_cup()
+        if self._customer_wants_condiments():
+            self._add_condiments()
+
+    @abstractmethod
+    def _brew(self):
+        pass
+
+    @abstractmethod
+    def _add_condiments(self):
+        pass
+
+    @final
+    def _boil_water(self):
+        print("Boiling water")
+
+    @final
+    def _pour_in_cup(self):
+        print("Pouring into cup")
+
+    def _customer_wants_condiments(self)-> None:
+        return None
