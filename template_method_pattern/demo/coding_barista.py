8dd2fe880de886b21cabd6fde38c9fa12bd55bbe
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-11-19 21:26:20 +0000

    Update Coding Barista Demo with Beverages with Hooks implementation

--- template_method_pattern/demo/coding_barista.py
+++ template_method_pattern/demo/coding_barista.py
@@ -1,10 +1,11 @@
-from coffee import Coffee
-from tea import Tea
+from coffee import Coffee, CoffeeWithHook
+from tea import Tea, TeaWithHook
 
 from termcolor import colored
 
 if __name__ == '__main__':
     print(colored('------------- TEMPLATE METHOD PATTERN -------------', 'white'))
+    print(colored('----------------- SIMPLE REQUEST ------------------', 'white'))
     print(colored('------------------- TEA REQUEST -------------------', 'blue'))
     tea = Tea()
     tea.prepare_recipe()
@@ -12,3 +13,12 @@ if __name__ == '__main__':
     print(colored('\n------------------ COFFEE REQUEST -----------------', 'green'))
     coffee = Coffee()
     coffee.prepare_recipe()
+
+    print(colored('\n---------- CONDIMENTS SPECIFIED REQUEST -----------', 'white'))
+    print(colored('------------------- TEA REQUEST -------------------', 'blue'))
+    tea_with_hook = TeaWithHook()
+    tea_with_hook.prepare_recipe()
+
+    print(colored('\n------------------ COFFEE REQUEST -----------------', 'green'))
+    coffee_with_hook = CoffeeWithHook()
+    coffee_with_hook.prepare_recipe()
