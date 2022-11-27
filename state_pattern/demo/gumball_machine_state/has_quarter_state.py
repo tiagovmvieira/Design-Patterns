a624feaa25b257a6d6efc8f2c0145532acd577b1
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-11-27 22:42:46 +0000

    Add HasQuarterState class included on the Gumball Machine State class

---
+++ state_pattern/demo/gumball_machine_state/has_quarter_state.py
@@ -0,0 +1,19 @@
+from state import State
+
+class HasQuarterState(State):
+    def __init__(self, gumball_machine):
+        self.gumball_machine = gumball_machine
+
+    def insert_quarter(self):
+        print("You can't insert another quarter")
+
+    def eject_quarter(self):
+        print("Quarter returned")
+        self.gumball_machine.set_state(self.gumball_machine._get_no_quarter_state())
+
+    def turn_cranck(self):
+        print("You turned...")
+        self.gumball_machine.set_state(self.gumball_machine._get_sold_state())
+
+    def dispense(self):
+        print("You need need to turn the crank")
