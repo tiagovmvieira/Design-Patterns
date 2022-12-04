e3eb57c3acb54eda52f15760ad0558b2ac3af4eb
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-04 22:18:45 +0000

    Add GooseDuckFactory class

---
+++ compound_patterns/demo/factory/goose_duck_factory.py
@@ -0,0 +1,13 @@
+from ducks.goose_duck import GooseDuck
+from adapter.goose_adapter import GooseDuckAdapter
+
+from quackable import Quackable
+
+class GooseDuckFactory:
+    def __init__(self)-> None:
+        pass
+
+    def create_goose_duck(self)-> Quackable:
+        return GooseDuckAdapter(GooseDuck())
+
+    
