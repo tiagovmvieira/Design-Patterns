5f91feb0e8f55cb8bc0e9bdbf5b63564e825b019
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-04 19:15:56 +0000

    Refactor DuckSimulator class to include QuackCounter decorator

--- compound_patterns/demo/duck_simulator.py
+++ compound_patterns/demo/duck_simulator.py
@@ -1,11 +1,13 @@
+from decorator.quack_counter import QuackCounter
+from quackable import Quackable
+
 from ducks.mallard_duck import MallardDuck
 from ducks.red_head_duck import RedHeadDuck
 from ducks.duck_call import DuckCall
 from ducks.rubber_duck import RubberDuck
 from ducks.goose_duck import GooseDuck
 
-from quackable import Quackable
-from goose_adapter import GooseDuckAdapter
+from adapter.goose_adapter import GooseDuckAdapter
 
 class DuckSimulator:
     def __init__(self)-> None:
@@ -13,10 +15,10 @@ class DuckSimulator:
 
     def simulate(self, duck: Quackable = None):
         if duck is None:
-            self.mallard_duck: Quackable = MallardDuck()
-            self.red_head_duck: Quackable = RedHeadDuck()
-            self.duck_call: Quackable = DuckCall()
-            self.rubber_duck: Quackable = RubberDuck()
+            self.mallard_duck: Quackable = QuackCounter(MallardDuck())
+            self.red_head_duck: Quackable = QuackCounter(RedHeadDuck())
+            self.duck_call: Quackable = QuackCounter(DuckCall())
+            self.rubber_duck: Quackable = QuackCounter(RubberDuck())
             self.goose_duck: Quackable = GooseDuckAdapter(GooseDuck())
 
             print("\nDuck Simulator")
@@ -25,5 +27,7 @@ class DuckSimulator:
             self.simulate(self.duck_call)
             self.simulate(self.rubber_duck)
             self.simulate(self.goose_duck)
+
+            print("The ducks quacked " + str(QuackCounter.get_quacks()) + " times")
         else:
             duck.quack()
