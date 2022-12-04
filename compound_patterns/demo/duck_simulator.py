8b7ab9ea44e0434be0a1a805337136ed12584782
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-04 22:19:16 +0000

    Refactor DuckSimulator class to include goose duck factory

--- compound_patterns/demo/duck_simulator.py
+++ compound_patterns/demo/duck_simulator.py
@@ -5,29 +5,32 @@ from ducks.goose_duck import GooseDuck
 
 from adapter.goose_adapter import GooseDuckAdapter
 
-from factory.abstract_duck_factory import AbstractDuckFactory
+from factory.abstract_duck_factory import AbstractDuckFactory 
+from factory.goose_duck_factory import GooseDuckFactory
 
 class DuckSimulator:
     def __init__(self)-> None:
         pass
 
-    def simulate(self, duck_factory: AbstractDuckFactory, duck: Quackable = None)-> None:
-        if duck is None and duck_factory is not None:
+    def simulate(self, duck_factory: AbstractDuckFactory, goose_duck_factory: GooseDuckFactory, duck: Quackable = None)-> None:
+        if duck is None and duck_factory is not None and goose_duck_factory is not None:
             self.duck_factory = duck_factory
+            self.goose_duck_factory = goose_duck_factory
 
             self.mallard_duck: Quackable = self.duck_factory.create_mallard_duck()
             self.red_head_duck: Quackable = self.duck_factory.create_red_head_duck()
             self.duck_call: Quackable = self.duck_factory.create_duck_call()
             self.rubber_duck: Quackable = self.duck_factory.create_rubber_duck()
-            self.goose_duck: Quackable = GooseDuckAdapter(GooseDuck())
+            self.goose_duck: Quackable = self.goose_duck_factory.create_goose_duck()
 
             print("\nDuck Simulator")
             duck_factory=None
-            self.simulate(duck_factory, self.mallard_duck)
-            self.simulate(duck_factory, self.red_head_duck)
-            self.simulate(duck_factory, self.duck_call)
-            self.simulate(duck_factory, self.rubber_duck)
-            self.simulate(duck_factory, self.goose_duck)
+            goose_duck_factory = None
+            self.simulate(duck_factory, goose_duck_factory, self.mallard_duck)
+            self.simulate(duck_factory, goose_duck_factory, self.red_head_duck)
+            self.simulate(duck_factory, goose_duck_factory, self.duck_call)
+            self.simulate(duck_factory, goose_duck_factory, self.rubber_duck)
+            self.simulate(duck_factory, goose_duck_factory, self.goose_duck)
 
             print("The ducks quacked " + str(QuackCounter.get_quacks()) + " times")
         else:
--- compound_patterns/demo/duck_simulator_test.py
+++ compound_patterns/demo/duck_simulator_test.py
@@ -1,5 +1,6 @@
 from duck_simulator import DuckSimulator
 from factory.counting_duck_factory import CountingDuckFactory
+from factory.goose_duck_factory import GooseDuckFactory
 
 from termcolor import colored
 
@@ -7,4 +8,5 @@ if __name__ == '__main__':
     print(colored('------------------ COMPOUND PATTERNS ----------------', 'white'))
     duck_simulator = DuckSimulator()
     duck_factory = CountingDuckFactory()
-    duck_simulator.simulate(duck_factory)
+    goose_duck_factory = GooseDuckFactory()
+    duck_simulator.simulate(duck_factory, goose_duck_factory)
