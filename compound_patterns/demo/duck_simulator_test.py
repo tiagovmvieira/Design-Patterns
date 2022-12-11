c4d71f6d7265fb4eeb0c878a99c62089f25d6d11
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-11 23:04:13 +0000

    Refactor duck simulator test to include all tests to be performed on the simulator

--- compound_patterns/demo/duck_simulator_test.py
+++ compound_patterns/demo/duck_simulator_test.py
@@ -6,7 +6,23 @@ from termcolor import colored
 
 if __name__ == '__main__':
     print(colored('------------------ COMPOUND PATTERNS ----------------', 'white'))
+    print(colored('---------------- SIMPLE DUCK SIMULATOR --------------', 'blue'))
     duck_simulator = DuckSimulator()
+    duck_simulator.simulate(initial=True)
+
+    print(colored('---------------- USING A DUCK ADAPTER ---------------', 'red'))
+    duck_simulator.simulate(adapter=True)
+
+    print(colored('------------ USING A QUACKABLE DECORATOR ------------', 'green'))
+    duck_simulator.simulate(decorator=True, adapter=True)
+
+    print(colored('------------- USING ABSTRACT FACTORIES --------------', 'yellow'))
     duck_factory = CountingDuckFactory()
     goose_duck_factory = GooseDuckFactory()
-    duck_simulator.simulate(duck_factory, goose_duck_factory)
+    duck_simulator.simulate(duck_factory=duck_factory, goose_duck_factory=goose_duck_factory)
+
+    print(colored('------------- USING FLOCKS COMPOSITES ---------------', 'cyan'))
+    duck_simulator.simulate(duck_factory=duck_factory, goose_duck_factory=goose_duck_factory, composite=True)
+
+    print(colored('---------- USING A QUACKOLOGIST OBSERVER ------------', 'magenta'))
+    duck_simulator.simulate(duck_factory=duck_factory, goose_duck_factory=goose_duck_factory, composite=True, observer=True)
