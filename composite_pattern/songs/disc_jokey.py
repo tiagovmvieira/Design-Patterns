b2b781ebb323e72ad75e3e8f4bed22c24c655402
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-11 15:55:04 +0000

    Refactor DiscJokey class

--- composite_pattern/songs/disc_jokey.py
+++ composite_pattern/songs/disc_jokey.py
@@ -6,14 +6,14 @@ from termcolor import colored
 
 if __name__ == '__main__':
     print(colored('------------------ COMPOSITE PATTERN ----------------', 'white'))
-    industrial_music: SongComponent = SongGroup("Industrial", "is a style of experimental music that draws on transgressive and \
-        provocative themes")
+    industrial_music: SongComponent = SongGroup("Industrial", "is a style of experimental music that draws on transgressive "\
+        "and provocative themes")
 
-    heavy_metal_music: SongComponent = SongGroup("\nHeavy Metal", "is a genre of rock that developed in the late 1960s, largely \
-        in the UK and in the US")
+    heavy_metal_music: SongComponent = SongGroup("\nHeavy Metal", "is a genre of rock that developed in the late 1960s, largely "\
+        "in the UK and in the US")
     
-    dubstep_music: SongComponent = SongGroup("\nDubstep", "is a genre of eletronic dance music that originated in South London \
-        England")
+    dubstep_music: SongComponent = SongGroup("\nDubstep", "is a genre of eletronic dance music that originated in South London "\
+        "England")
 
     every_song: SongComponent = SongGroup("Song List", "Every Song Available")
     every_song.add(industrial_music)
