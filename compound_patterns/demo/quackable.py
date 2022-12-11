634f84ee27884465cf38a73e0ca4c80370f2b0db
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-11 23:08:34 +0000

    Refactor Quackable interface to include observer design logic

--- compound_patterns/demo/quackable.py
+++ compound_patterns/demo/quackable.py
@@ -1,6 +1,8 @@
+from observer.quack_observable import QuackObservable
+
 from abc import ABC, abstractmethod
 
-class Quackable(ABC):
+class Quackable(QuackObservable, ABC):
     @abstractmethod
     def quack(self):
         pass
