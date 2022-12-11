b98cecf24f0a805460742e46e483ce7e27b70916
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-11 23:07:18 +0000

    Refactor GooseAdapter class to include observer design logic

--- compound_patterns/demo/adapter/goose_adapter.py
+++ compound_patterns/demo/adapter/goose_adapter.py
@@ -1,9 +1,22 @@
 from quackable import Quackable
 from ducks.goose_duck import GooseDuck
+from observer.observable import Observable
+from observer.observer import Observer
 
 class GooseDuckAdapter(Quackable):
-    def __init__(self, goose: GooseDuck):
+    def __init__(self, goose: GooseDuck)-> None:
         self.goose = goose
+        self.observable = Observable(self)
 
-    def quack(self):
+    def __str__(self)-> str:
+        return "Goose pretending to be a Duck"
+
+    def notify_observers(self)-> None:
+        self.observable.notify_observers()
+
+    def quack(self)-> None:
         self.goose.honk()
+        self.notify_observers()
+
+    def register_observer(self, observer: Observer)-> None:
+        self.observable.register_observer(observer)
