02ffaef9f75bbdb9cee0c52f51ca8a44cd112569
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-12 23:19:41 +0000

    Add MenuItem class included on the MenuTestDrive demo class

---
+++ composite_pattern/demo/menu_item.py
@@ -0,0 +1,27 @@
+from menu_component import MenuComponent
+
+class MenuItem(MenuComponent):
+    get_name, get_description, get_price, is_vegetarian = 'yes', 'yes', 'yes', 'yes'
+    add, get_child, remove = 'no', 'no', 'no'
+
+    def __init__(self, name: str, description: str, vegetarian: bool, price: float):
+        super().__init__()
+        self.name=name
+        self.description=description
+        self.vegetarian=vegetarian
+        self.price=price
+
+    def get_name(self)-> str:
+        return self.name
+
+    def get_description(self)-> str:
+        return self.description
+
+    def get_price(self)-> float:
+        return self.price
+
+    def is_vegetarian(self) -> bool:
+        return self.vegetarian
+
+    def print(self):
+        print(" ", self.get_name(), "(v)" if self.is_vegetarian else "", ", " + str(self.get_price()), "     -- " + self.get_description())
