49ef0fdbf8da22ee19defeef589a5a2193080fab
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-14 23:20:21 +0000

    Update Controller class with on_button_click method

--- compound_patterns/calculator/controller.py
+++ compound_patterns/calculator/controller.py
@@ -1,6 +1,8 @@
 from model import Model
 from view import View
 
+from typing import Union
+
 class Controller:
     def __init__(self)-> None:
         self.model: Model = Model()
@@ -8,6 +10,11 @@ class Controller:
   
     def main(self):
         self.view.main()
+
+    def on_button_click(self, caption: Union[str, int]):
+        result = self.model.calculate(caption)
+
+        self.view.value_variable.set(result)
 
 if __name__ == '__main__':
     calculator = Controller()
