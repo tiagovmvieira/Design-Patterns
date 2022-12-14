e6ed957846818123a22d46dfa8d07400a047d5aa
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-14 23:21:26 +0000

    Update Model class with calculate method

--- compound_patterns/calculator/model.py
+++ compound_patterns/calculator/model.py
@@ -1,3 +1,13 @@
+from typing import Union
+
 class Model:
     def __init__(self):
-        pass
+        self.value = ''
+
+    def calculate(self, caption: Union[str, int])-> str:
+        if caption == 'C':
+            self.value = ''
+        elif isinstance(caption, int):
+            self.value += str(caption)
+
+        return self.value
