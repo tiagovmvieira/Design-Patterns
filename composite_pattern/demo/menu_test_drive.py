95b35dd5beee9e64f812c253c9db9fa1e2f28049
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-16 20:16:46 +0000

    Refactor MenuTestDrive class to include new menu items according to the demo

--- composite_pattern/demo/menu_test_drive.py
+++ composite_pattern/demo/menu_test_drive.py
@@ -19,6 +19,69 @@ if __name__ == '__main__':
     all_menus.add(diner_menu)
     all_menus.add(cafe_menu)
 
+    pancake_house_menu.add((MenuItem(
+        "K&B's Pancake Breakfast",
+        "Pancakes with scrambled eggs and toast",
+        True,
+        2.99
+    )))
+
+    pancake_house_menu.add((MenuItem(
+        "Regular Pancake Breakfast",
+        "Pancakes with fried eggs, sausage",
+        False,
+        2.99
+    )))
+
+    pancake_house_menu.add((MenuItem(
+        "Blueberry Pancakes",
+        "Pancakes made with fresh blueberries, and blueberry syrup",
+        True,
+        3.49
+    )))
+
+    pancake_house_menu.add((MenuItem(
+        "Waffles",
+        "Waffles with your choice of blueberries or strawberries",
+        True,
+        3.59
+    )))
+
+    diner_menu.add(MenuItem(
+        "Vegetarian BLT",
+        "(Fakin') Bacon with lettuce & tomato on whole wheat",
+        True,
+        2.99
+    ))
+
+    diner_menu.add(MenuItem(
+        "BLT",
+        "Bacon with lettuce & tomato on whole wheat",
+        False,
+        2.99
+    ))
+
+    diner_menu.add(MenuItem(
+        "Soup of the day",
+        "A bowl of the soup of the day, with a side of potato salad",
+        False,
+        3.29
+    ))
+
+    diner_menu.add(MenuItem(
+        "Hot Dog",
+        "A hot dog, with sauerkraut, relish, onions, topped with cheese",
+        False,
+        3.05
+    ))
+
+    diner_menu.add(MenuItem(
+        "Steamed Veggies and Brown Rice",
+        "Steamed vegetables over brown rice",
+        True,
+        3.99
+    ))
+
     diner_menu.add(MenuItem(
         "Pasta",
         "Spaghetti with Marinara Sauce, and a slice of sourdough bread",
@@ -33,6 +96,41 @@ if __name__ == '__main__':
         "Apple Pie with a flakey crust, topped with vanilla ice cream",
         True,
         1.59
+    ))
+
+    dessert_menu.add(MenuItem(
+        "Cheesecake",
+        "Creamy New York cheesecake, with a chocolate graham crust",
+        True,
+        1.99
+    ))
+
+    dessert_menu.add(MenuItem(
+        "Sorbet",
+        "A scoop of raspberry and a coop of lime",
+        True,
+        1.89
+    ))
+
+    cafe_menu.add(MenuItem(
+        "Veggie Burguer and Air Fries",
+        "Veggie burger on a whole wheat bun, lettuce, tomato, and fries",
+        True,
+        3.99
+    ))
+
+    cafe_menu.add(MenuItem(
+        "Soup of the day",
+        "A cup of the soup of the day, with a side salad",
+        False,
+        3.69
+    ))
+
+    cafe_menu.add(MenuItem(
+        "Burrito",
+        "A large burrito, with whole pinto beans, salsa, guacamole",
+        True,
+        4.29
     ))
 
     waitress: Waitress = Waitress(all_menus)
