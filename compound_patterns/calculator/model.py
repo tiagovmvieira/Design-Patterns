54a332f05dae4239fa1061cb4cd6cc8028d1bca1
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-17 20:26:23 +0000

    Update Model class calculate method

--- compound_patterns/calculator/model.py
+++ compound_patterns/calculator/model.py
@@ -2,12 +2,35 @@ from typing import Union
 
 class Model:
     def __init__(self):
-        self.value = ''
+        self.previous_value: str = ''
+        self.value: str = ''
+        self.operator: str = ''
 
     def calculate(self, caption: Union[str, int])-> str:
         if caption == 'C':
+            self.previous_value = ''
             self.value = ''
+            self.operator = ''
+        elif caption == '+/-':
+            self.value = str(int(self.value) * -1)
+        elif caption == '%':
+            self.value = str(int(self.value) / 100)
+        elif caption == '.':
+            if not caption in self.value:
+                self.value += caption
+        elif caption == '=':
+            self.value = str(self._evaluate())
+
         elif isinstance(caption, int):
             self.value += str(caption)
+        else:
+            if self.value:
+                if caption == '/' and isinstance(self.value, int):
+                    self.operator = caption
+                    self.previous_value = self.value
+                self.value = ''
 
         return self.value
+
+    def _evaluate(self)-> float:
+        return eval(self.previous_value + self.operator + self.value)
